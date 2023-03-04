05b6e4d196de99290060f92daaac16c4f0b5ce35
007juanca <jgarciam114@miumg.edu.gt>
2023-03-03 19:54:11 -0600

    Agregar archivos de proyecto.

---
+++ desafio_1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace desafio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("estimado usuario ¿cómo se llama? ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("colocar los ingresos de los ultimos 3 meses");
+
+            Console.WriteLine("antepenultimo ingreso: ");
+            int Ingreso1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("penultimo ingreso: ");
+            int Ingreso2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ultimo ingreso: ");
+            int Ingreso3 = int.Parse(Console.ReadLine());
+
+            int suma = Ingreso1 + Ingreso2 + Ingreso3;
+            int promedio = suma / 3;
+
+            Console.WriteLine("señor@: " + nombre);
+            Console.WriteLine("el total de ingresos es de: Q" + suma);
+            Console.WriteLine("el promedio de los ultimos 3 meses es de: Q" + promedio);
+
+            Console.Read();
+        }
+    }
+}
